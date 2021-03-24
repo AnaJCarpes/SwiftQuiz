79f6bc63792f8c4c204a7478e3646530d354136f
Ana Paula <anajcarpes@gmail.com>
2021-03-24 13:40:23 -0300

    Initial Commit

---
+++ SwiftQuiz/Quiz.swift
@@ -0,0 +1,8 @@
+//
+//  Quiz.swift
+//  SwiftQuiz
+//
+//  Created by Ana Jensen on 25/03/21.
+//
+
+import Foundation
---
+++ SwiftQuiz/QuizManager.swift
@@ -0,0 +1,8 @@
+//
+//  QuizManager.swift
+//  SwiftQuiz
+//
+//  Created by Ana Jensen on 25/03/21.
+//
+
+import Foundation
